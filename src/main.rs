834e3d4b2e0a3ce42022b2a1b07ee62d06cf0dbf
Cfp <chaosflo44plus@gmail.com>
2024-11-04 17:18:12 +0100

    ✨ Failover mode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 const LAN_INTERFACE: &str = "eth1"; // TODO: Make this configurable
+const ROUTER_IP: &str = "10.10.99.1"; // TODO: Make this configurable
 
 fn start_service(name: &str) {
 	println!("[failoverd] Starting service: {}", name);
@@ -15,7 +16,8 @@ fn start_service(name: &str) {
 	}
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
 	println!("[failoverd] Starting!");
 	// Bring up the LAN interface
 	println!("[failoverd] Bringing up LAN interface: {}", LAN_INTERFACE);
@@ -28,20 +30,57 @@ fn main() {
 		.arg(LAN_INTERFACE)
 		.arg("-n")
 		.arg("-q")
-		.arg("-R")
+		// .arg("-R")
 		.spawn()
 		.expect("Failed to run DHCP client!");
 	let dhcp_client_status = dhcp_client.wait().expect("Failed to wait for DHCP client!");
-	interface.set_up(false).expect("Failed to bring down LAN interface!");
 	if dhcp_client_status.success() {
 		println!("[failoverd] DHCP server found on LAN interface: {}", LAN_INTERFACE);
 		// Need to start the failover routing process
-		// TODO
-		unimplemented!();
+		failover().await;
 	} else {
 		println!("[failoverd] No DHCP server found on LAN interface: {}", LAN_INTERFACE);
+		interface.set_up(false).expect("Failed to bring down LAN interface!");
 		// Need to start the regular routing process
-		start_service("networking");
-		start_service("dhcpd");
+		routing(); // This will never return
+	}
+}
+
+fn routing() -> ! {
+	start_service("networking");
+	start_service("dhcpd");
+	println!("We are in routing mode!");
+	loop {}
+}
+
+async fn failover() -> ! {
+	let ping_payload = [0; 8];
+	loop {
+		let ping = surge_ping::ping(ROUTER_IP.parse().unwrap(), &ping_payload).await;
+
+		if ping.is_err() {
+			println!("Router did not respond to ping: {:?}", ping.err());
+			tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+			let ping = surge_ping::ping(ROUTER_IP.parse().unwrap(), &ping_payload).await;
+
+			if ping.is_ok() {
+				println!("Router responded: {:?}", ping.unwrap().1.as_millis());
+				tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+				continue;
+			}
+
+			println!("Router still did not respond to ping: {:?}. Assuming it is dead.", ping.err());
+
+			let mut interface = interfaces::Interface::get_by_name(LAN_INTERFACE).unwrap().expect("LAN interface not found! Is the interface name correct?");
+			interface.set_up(false).expect("Failed to bring down LAN interface!");
+			routing();
+		}
+
+		let ping = ping.unwrap();
+
+		println!("Ping to Router: {}ms", ping.1.as_millis());
+
+		tokio::time::sleep(std::time::Duration::from_secs(10)).await;
 	}
 }
